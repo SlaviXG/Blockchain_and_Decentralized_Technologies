b4753ce995009424ff63d4e78fc6a473f221147a
Vyacheslav <slavixg409@gmail.com>
2022-05-28 22:58:21 +0300

    Wrapped with the main function

--- Practice 2/main.py
+++ Practice 2/main.py
@@ -1,12 +1,17 @@
 import hexIntConverter
 
-n = 255#input()
+
+val = input("\ni.\tValue: ")
+print("ii.\tNumber of bytes: ", (len(val)-2)//2)
+print("iii.Little-endian:	", hexIntConverter.hex_to_little_endian(val))
+print("iv.\tBig-endian: ", hexIntConverter.hex_to_big_endian(val))
+
+
+
+### Testing functions :
 
 #print(hexIntConverter.hex_to_big_endian(n))
-
 #print(hexIntConverter.hex_to_little_endian(n))
-
 #print(len(hexIntConverter.big_endian_to_hex(n)))
 #print(hexIntConverter.big_endian_to_hex(n))
-
-print(hexIntConverter.little_endian_to_hex(n, 32))
+#print(hexIntConverter.little_endian_to_hex(n, 32))
