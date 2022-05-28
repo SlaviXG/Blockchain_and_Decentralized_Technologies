a5539eb1962d460bebdafbd9be7a9441fd75a6ae
Vyacheslav <slavixg409@gmail.com>
2022-05-29 00:20:23 +0300

    Implemented byte-array realization

--- Practice 2/hexIntConverter.py
+++ Practice 2/hexIntConverter.py
@@ -3,13 +3,24 @@ def hex_to_big_endian(hex_value):
 
 def hex_to_little_endian(hex_value):
     hex_value = hex_value.lstrip("0x")
-    return int(str(hex_value[::-1]), 16)
+    byte_array = bytearray.fromhex(hex_value)
+    byte_array.reverse()
+    hex_value = ''.join(format(i, '02x') for i in byte_array)
+    return int(hex_value, 16)
 
 def big_endian_to_hex(big_endian):
     return hex(int(big_endian))
 
 def little_endian_to_hex(little_endian, number_of_bytes):
-    hex_str = hex(little_endian)
+    hex_str = hex(int(little_endian))
+    hex_str = hex_str.lstrip("0x")
+
+    byte_array = bytearray.fromhex(hex_str)
+    byte_array.reverse()
+    hex_str = ''.join(format(i, '02x') for i in byte_array)
+
+    hex_str = '0x' + hex_str
+
     for i in range(2 * number_of_bytes - len(hex_str) + 2):
         hex_str += '0'
 
--- Practice 2/main.py
+++ Practice 2/main.py
@@ -10,8 +10,8 @@ print("iv.\tBig-endian: ", hexIntConverter.hex_to_big_endian(val))
 
 ### Testing functions :
 
-#print(hexIntConverter.hex_to_big_endian(n))
+#print(hexIntConverter.hex_to_big_endian(2))
 #print(hexIntConverter.hex_to_little_endian(n))
 #print(len(hexIntConverter.big_endian_to_hex(n)))
-#print(hexIntConverter.big_endian_to_hex(n))
-#print(hexIntConverter.little_endian_to_hex(n, 32))
+#print(hexIntConverter.big_endian_to_hex(255))
+#print(hexIntConverter.little_endian_to_hex(255, 32))
