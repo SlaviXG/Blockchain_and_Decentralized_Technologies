008e2db42089c7f13740dbe00b38cabca621bd16
Vyacheslav <slavixg409@gmail.com>
2022-06-24 23:07:01 +0300

    Created Key Pair

---
+++ Blockchain for Digital Identity/KeyPair.cpp
@@ -0,0 +1,5 @@
+//
+// Created by SlaviX on 24.06.2022.
+//
+
+#include "KeyPair.h"
---
+++ Blockchain for Digital Identity/KeyPair.h
@@ -0,0 +1,13 @@
+#ifndef BLOCKCHAIN_FOR_DIGITAL_IDENTITY_KEYPAIR_H
+#define BLOCKCHAIN_FOR_DIGITAL_IDENTITY_KEYPAIR_H
+
+
+class KeyPair
+{
+private:
+    unsigned int privateKey;
+
+};
+
+
+#endif
